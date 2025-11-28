b74e5d73d99c78180865c9035b2ea509f62b2d00
Masato Shuto <154576364+ms-yozakura@users.noreply.github.com>
2025-11-28 10:20:44 +0900

    refactor: 本格的にHPのレイアウトを変えていきたい所存

--- js/swiper_init.js
+++ js/swiper_init.js
@@ -1,7 +1,7 @@
 
 // 1. 初期化の対象となるコンテナのセレクタを指定
 //    ここではHTMLで定義したクラス名「.swiper」を指定
-const mySwiper = new Swiper(".swiper", {
+const mySwiper = new Swiper("#swiper1", {
     // 2. ここに各種オプションを設定 (例: スライドモード、スピード、ループなど)
 
     // オプション例:
@@ -12,11 +12,11 @@ const mySwiper = new Swiper(".swiper", {
     spaceBetween: 30, // スライド間のスペース (px)
 
     effect: 'coverflow', // エフェクトの種類
-coverflowEffect: {
-  rotate: 50, // 傾き
-  slideShadows: false, // 影を有効化
-}
-,
+    coverflowEffect: {
+        rotate: 50, // 傾き
+        slideShadows: false, // 影を有効化
+    }
+    ,
     autoplay: {
         delay: 3000, // 自動再生の遅延時間 (ミリ秒)
         disableOnInteraction: false, // ユーザー操作後も自動再生を継続
@@ -29,9 +29,9 @@ coverflowEffect: {
     //     clickable: true, // クリックで移動を有効化
     // },
     scrollbar: {
-    el: '.swiper-scrollbar', // HTML要素のセレクタを指定
-    hide: true,              // 操作時以外は非表示にするか
-  },
+        el: '.swiper-scrollbar', // HTML要素のセレクタを指定
+        hide: true,              // 操作時以外は非表示にするか
+    },
     navigation: {
         nextEl: ".custom-next-btn", // 次へボタンの要素を指定
         prevEl: ".custom-prev-btn", // 前へボタンの要素を指定
@@ -50,6 +50,57 @@ coverflowEffect: {
             slidesPerView: 1,
             spaceBetween: 50,
             centeredSlides: true,
+        },
+    },
+});
+
+
+
+
+// 1. 初期化の対象となるコンテナのセレクタを指定
+//    ここではHTMLで定義したクラス名「.swiper」を指定
+const mySwiper2 = new Swiper("#swiper2", {
+    // 2. ここに各種オプションを設定 (例: スライドモード、スピード、ループなど)
+
+    // オプション例:
+    direction: "horizontal", // スライドの方向: 'horizontal' または 'vertical'
+    loop: true, // ループ再生を有効にする
+    slidesPerView: 2.5, // 一度に表示するスライドの枚数
+    spaceBetween: 0, // スライド間のスペース (px)
+    centeredSlides: true,
+
+    effect: 'slides', // エフェクトの種類
+    autoplay: {
+        delay: 3000, // 自動再生の遅延時間 (ミリ秒)
+        disableOnInteraction: false, // ユーザー操作後も自動再生を継続
+    },
+    speed: 800, // スライドの切り替え速度 (ミリ秒)
+
+    // 3. ナビゲーション要素の関連付け
+    // pagination: {
+    //     el: ".swiper-pagination", // ページネーションの要素を指定
+    //     clickable: true, // クリックで移動を有効化
+    // },
+    scrollbar: {
+        el: '.swiper-scrollbar', // HTML要素のセレクタを指定
+        hide: true,              // 操作時以外は非表示にするか
+    },
+    navigation: {
+        nextEl: ".custom-next-btn", // 次へボタンの要素を指定
+        prevEl: ".custom-prev-btn", // 前へボタンの要素を指定
+    },
+
+    // 4. レスポンシブ設定 (ブレイクポイント)
+    breakpoints: {
+        // 768px以上の画面幅で適用される設定
+        768: {
+            slidesPerView: 3,
+            // spaceBetween: 40,
+        },
+        // 1024px以上の画面幅で適用される設定
+        1024: {
+            slidesPerView: 5,
+            // spaceBetween: 50,
         },
     },
 });
